334cdc8ec27c19ca37883e012529cbf70a03b85e
Halil Erkan Dursunoğlu <h.erkan.dursunoglu@gmail.com>
2020-08-16 21:58:16 +0300

    kadin cinayetleri ve komşularla sorun sayisi affiniza siginarak guncellendi.

--- ulke.js
+++ ulke.js
@@ -1,40 +1,40 @@
-
 const ulke = {
-	adalet: () => undefined,
-	
-	ekonomi: () => undefined,
-	
-	insanHaklari: () => undefined,
-	
-	basinOzgurlugu: () => undefined,
-	
-	kadinCinayetleri: () => Number.MAX_SAFE_INTEGER,
-	
-	sanat: () => undefined,
-	
-	bilim: () => undefined,
-	
-	komsularlaSorunSayisi: () => Number.MAX_SAFE_INTEGER,
-	
-	dolardanEndiselenenVarsa: () => "Birincisi şunu soriyim size. Dolarla mı maaş alıyorsunuz. Dolar borcunuz mu var? Dolarla bir işiniz mi var?",
-	
-	cinsiyetAyrimi: () => true,
-  
-  	tuikRakamlari: () => "Harikayız harika!",
-	
-	run() {
-		return {
-			adalet: this.adalet(),
-			ekonomi: this.ekonomi(),
-			insanHaklari: this.insanHaklari(),
-			basinOzgurlugu: this.basinOzgurlugu(),
-			kadinCinayetleri: this.kadinCinayetleri(),
-			sanat: this.sanat(),
-			bilim: this.bilim(),
-			komsularlaSorunSayisi: this.komsularlaSorunSayisi(),
-			dolardanEndiselenenVarsa: this.dolardanEndiselenenVarsa(),
-			cinsiyetAyrimi: this.cinsiyetAyrimi(),
-			tuikRakamlari: this.tuikRakamlari(),
-		}
-	}
-}
+  adalet: () => undefined,
+
+  ekonomi: () => undefined,
+
+  insanHaklari: () => undefined,
+
+  basinOzgurlugu: () => undefined,
+
+  kadinCinayetleri: () => BigInt(Number.MAX_VALUE),
+
+  sanat: () => undefined,
+
+  bilim: () => undefined,
+
+  komsularlaSorunSayisi: () => BigInt(Number.MAX_VALUE),
+
+  dolardanEndiselenenVarsa: () =>
+    'Birincisi şunu soriyim size. Dolarla mı maaş alıyorsunuz. Dolar borcunuz mu var? Dolarla bir işiniz mi var?',
+
+  cinsiyetAyrimi: () => true,
+
+  tuikRakamlari: () => 'Harikayız harika!',
+
+  run() {
+    return {
+      adalet: this.adalet(),
+      ekonomi: this.ekonomi(),
+      insanHaklari: this.insanHaklari(),
+      basinOzgurlugu: this.basinOzgurlugu(),
+      kadinCinayetleri: this.kadinCinayetleri(),
+      sanat: this.sanat(),
+      bilim: this.bilim(),
+      komsularlaSorunSayisi: this.komsularlaSorunSayisi(),
+      dolardanEndiselenenVarsa: this.dolardanEndiselenenVarsa(),
+      cinsiyetAyrimi: this.cinsiyetAyrimi(),
+      tuikRakamlari: this.tuikRakamlari(),
+    };
+  },
+};
